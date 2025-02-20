413686f39d3d30a61399e7c265d71c9ee24fe42e
Pluk Zwaal <40401658+PlukZwaal@users.noreply.github.com>
2025-02-20 12:16:28 +0100

    Removed user db thing

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,26 +1,33 @@
-﻿using WebAPI.Controllers;
+﻿using Microsoft.AspNetCore.Identity;
+using WebAPI.Controllers;
 using WebAPI.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var sqlConnectionString = builder.Configuration["SqlConnectionString"];
 var sqlConnectionStringFound = !string.IsNullOrEmpty(sqlConnectionString);
 
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+    options.Password.RequiredLength = 50;
+})
+.AddRoles<IdentityRole>()
+.AddDapperStores(options =>
+{
+    options.ConnectionString = sqlConnectionString;
+});
+
 builder.Services.AddTransient<Environment2DRepository, Environment2DRepository>(o => new Environment2DRepository(sqlConnectionString));
 builder.Services.AddTransient<Object2DRepository, Object2DRepository>(o => new Object2DRepository(sqlConnectionString));
-builder.Services.AddTransient<UserRepository, UserRepository>(o => new UserRepository(sqlConnectionString));
-
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -32,7 +39,7 @@ app.MapGet("/", () => $"The API is up . Connection string found: {(sqlConnection
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
-app.MapControllers();
+app.MapGroup("/account").MapIdentityApi<IdentityUser>();
+app.MapControllers().RequireAuthorization();
 
 app.Run(); 
